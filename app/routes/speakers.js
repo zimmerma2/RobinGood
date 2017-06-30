cd7e3ad88558358389195f31a47defbf3a11a363
Aleksandar Hrusanov <hrusanov@grinnell.edu>
2017-06-30 10:12:10 +0300

    Following a cool and easy tutorial and adapting it to the project

---
+++ app/routes/speakers.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/speakers', function(req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pagespeakers = data.speakers;
+
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
+
+  res.render('speakers', {
+    pageTitle: 'speakers',
+    artwork: pagePhotos,
+    speakers: pagespeakers,
+    pageID: 'speakers'
+  });
+});
+
+router.get('/speakers/:speakerid', function(req, res) {
+  var dataFile = req.app.get('appData');
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+      <link rel="stylesheet" type="text/css" href="/css/style.css">
+      <h1>${speaker.title}</h1>
+      <h2>with ${speaker.name}</h2>
+      <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker">
+      <p>${speaker.summary}</p>
+      <script src="/reload/reload.js"></script>
+  `);
+});
+
+module.exports = router;
