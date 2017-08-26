13d27464ad3c15790443eb73d0d8a591ebcf26ed
Aleksandar Hrusanov <hrusanov@grinnell.edu>
2017-08-26 05:23:36 +0300

    Fixed post problem with signup:

--- app/routes/sponsorsignup.js
+++ app/routes/sponsorsignup.js
@@ -6,11 +6,11 @@ module.exports = function(app, passport) {
   app.get('/sponsorsignup', function(req, res) {
 
     // render the page and pass in any flash data if it exists
-    res.render('sponsorsignup.pug', { message: req.flash('signupMessage') });
+    res.render('sponsor/sponsorsignup.pug', { message: req.flash('signupMessage') });
   });
 
   // process the signup form
-  app.post('/sponsor/sponsorsignup', passport.authenticate('sponsor-local-signup', {
+  app.post('/sponsorsignup', passport.authenticate('sponsor-local-signup', {
     successRedirect : '/sponsorlogin', // redirect to the secure profile section
     failureRedirect : '/sponsorsignup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
