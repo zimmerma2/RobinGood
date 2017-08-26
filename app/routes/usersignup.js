d069ddbd66c5444f72bd938a1c1a706797c97b83
Aleksandar Hrusanov <hrusanov@grinnell.edu>
2017-08-26 06:25:49 +0300

    Signup works

--- app/routes/usersignup.js
+++ app/routes/usersignup.js
@@ -3,16 +3,29 @@ module.exports = function(app, passport) {
   // USER SIGNUP =========================
   // =====================================
   // show the signup form
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json());
+
   app.get('/usersignup', function(req, res) {
 
     // render the page and pass in any flash data if it exists
     res.render('user/usersignup.pug', { message: req.flash('signupMessage') });
   });
 
-  // process the signup form
+  // app.post('/usersignup', function(req, res, next) {
+  //     console.log(req.url);
+  //     passport.authenticate('user-local-login', function(err, user, info) {
+  //         console.log("authenticate");
+  //         console.log(err);
+  //         console.log(user);
+  //         console.log(info);
+  //     })(req, res, next);
+  // });
+
+// process the signup form
   app.post('/usersignup', passport.authenticate('user-local-signup', {
-    successRedirect : '/userlogin', // redirect to the secure profile section
-    failureRedirect : '/usersignup', // redirect back to the signup page if there is an error
+    successRedirect : '/usersignup', // redirect to the secure profile section
+    failureRedirect : '/userlogin', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 };
